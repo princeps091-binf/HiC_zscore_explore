145b0778c9e581c925187a73bfbd8bfe7b80a154
princeps091-binf <princeps091@gmail.com>
2021-12-21 14:35:35 +0100

    new zscore script

--- code/environment_setup.R
+++ code/environment_setup.R
@@ -2,4 +2,6 @@ library(renv)
 renv::init(project = "~/Documents/multires_bhicect/HiC_enrichment/HiC_zscore_explore/")
 
 renv::install("tidyverse")
+renv::install("furrr")
+renv::install("Matrix")
 renv::snapshot()
