e3528431a96d241e934672f5c7e6cb5b8d67ef88
princeps091-binf <princeps091@gmail.com>
2022-01-27 14:10:09 +0100

    processing unibind results

---
+++ code/unibind_res_process.R
@@ -0,0 +1,23 @@
+# Examine Unibind output
+library(tidyverse)
+options(scipen = 999999999)
+res_set <- c('1Mb','500kb','100kb','50kb','10kb','5kb')
+res_num <- c(1e6,5e5,1e5,5e4,1e4,5e3)
+names(res_num)<-res_set
+
+#-----------------------------------------
+unibind_folder<-"./data/Unibind/UniBind_oneSetBg_HMEC_50kb_hub_vs_HMEC_enh/"
+
+TF_LOLA_res_files<-grep("^col_",list.files(unibind_folder),value=T)
+
+breast_TF_l<-map(TF_LOLA_res_files,function(TF_file){
+  message(TF_file)
+  tmp_tbl<-read_delim(file=paste0(unibind_folder,TF_file),col_types = list(
+    collection = col_character(),
+    cellType = col_character()
+  ))
+  
+  return(tmp_tbl %>% filter(grepl("breast",description)) %>% dplyr::select(collection,pValueLog,oddsRatio,support,cellType))
+})
+
+breast_TF_tbl<-base::do.call(bind_rows,breast_TF_l)
